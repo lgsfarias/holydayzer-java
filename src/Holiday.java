9ccf1b91d0e2a0633a8cc83094bc6f2b071db956
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2023-01-13 15:28:28 -0300

    feat: create Holiday class

---
+++ src/Holiday.java
@@ -0,0 +1,17 @@
+public class Holiday {
+  private String name;
+  private String date;
+
+  public Holiday(String name, String date) {
+    this.name = name;
+    this.date = date;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDate() {
+    return date;
+  }
+}
